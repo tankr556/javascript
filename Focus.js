4a628f61e39b48af4fc95af1ebd6df56e565f264
tankr556 <tankr556@gmail.com>
2022-11-13 10:06:21 +0530

    java

---
+++ Focus.js
@@ -0,0 +1,10 @@
+function ok() {
+    ok = document.getElementById("in");
+    ok.style.backgroundColor = "green";
+    ok.style.color = "white";
+}
+
+function okk() {
+    ok = document.getElementById("btn");
+    ok.style.backgroundColor = "red";
+}
---
+++ support.js
@@ -0,0 +1,12 @@
+function val() {
+    document.getElementsByTagName("input")[0].style.backgroundColor = "red"
+    document.getElementsByTagName("input")[1].style.backgroundColor = "green"
+
+}
+
+
+function btn() {
+    var btn = document.getElementById("btn");
+    btn.style.color = "white";
+    btn.style.background = "black";
+}
